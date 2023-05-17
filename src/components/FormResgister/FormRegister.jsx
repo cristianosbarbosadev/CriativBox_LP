ccee577e159b39d9094a1d8e191264896188c32f
Cristiano Barbosa <cristianosbarbosa.dev@gmail.com>
2023-05-17 17:14:00 -0300

    pendencias

--- src/components/FormResgister/FormRegister.jsx
+++ src/components/FormResgister/FormRegister.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FormRegisterContainer,
   FormRegisterButton,
@@ -10,26 +10,86 @@ import Box from '@mui/material/Box';
 import { ThemeProvider, useTheme } from '@mui/material/styles';
 import { customTheme } from './FormRegisterElements';
 import TextField from '@mui/material/TextField';
-
-
-
+import axios from 'axios';
 
 function FormRegister() {
   const outerTheme = useTheme();
 
-  
   const handleClose = () => {
     window.location.reload();
   };
 
+  //Declaração de estado para os inputs
+
+  const [data, setData] = useState({
+    nomePizzaria: '',
+    nomeResponsavel: '',
+    telefone: '',
+    email: '',
+    endereco: '',
+    raioEntrega: '',
+    consumoMes: '',
+    consumoFimDeSemana: '',
+    valorMedio: '',
+    canaisVenda: '',
+  });
+
+  //Declarar a variavel para receber a mensagem
+  const [msg, setMsg] = useState('');
+
+  //Receber os dados dos inputs
+
+  const valorInput = e => setData({ ...data, [e.target.name]: e.target.value });
+
+  //Enviar os dados para o backend
+
+  const sendMsg = async e => {
+    e.preventDefault();
+    console.log(`Nome da Pizzaria: ${data.nomePizzaria}`);
+    console.log(`Nome do Responsavel: ${data.nomeResponsavel}`);
+    console.log(`Telefone: ${data.telefone}`);
+    console.log(`Email: ${data.email}`);
+    console.log(`Endereco: ${data.endereco}`);
+    console.log(`Raio de entrega: ${data.raioEntrega}`);
+    console.log(`Consumo mes: ${data.consumoMes}`);
+    console.log(`Consumo fim de semana: ${data.consumoFimDeSemana}`);
+    console.log(`Valor medio: ${data.valorMedio}`);
+    console.log(`Canais venda: ${data.canaisVenda}`);
+
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+    await axios.post('http://localhost:8080/cadastro', data, headers)
+      .then(response => {
+        setMsg(response.data.cadastro);
+        setData({
+          nomePizzaria: '',
+          nomeResponsavel: '',
+          telefone: '',
+          email: '',
+          endereco: '',
+          raioEntrega: '',
+          consumoMes: '',
+          consumoFimDeSemana: '',
+          valorMedio: '',
+          canaisVenda: '',
+        });
+      })
+      .catch(error => {
+        setMsg(error.response.data.cadastro);
+      });
+  };
   return (
-    <FormRegisterContainer >
+    <FormRegisterContainer>
       <Icon onClick={handleClose}>
         <CloseIcon />
       </Icon>
       <FormRegisterHeading>
         Falta pouco para garantir sua fatia de economia!
       </FormRegisterHeading>
+
+      {/* mensagem de sucesso ou erro  */}
+      {msg ? <p>{msg}</p> : ''}
 
       {/* Inputs */}
       <Box
@@ -41,48 +101,100 @@ function FormRegister() {
           justifyContent: 'center',
         }}
       >
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Nome da Pizzaria" type="Phone" />
-        </ThemeProvider>
+        <form onSubmit={sendMsg}>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              name="nomePizzaria"
+              label="Nome da Pizzaria"
+              type="text"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Nome do responsável" type="Phone" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Nome do responsável"
+              type="text"
+              name="nomeResponsavel"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Telefone" type="Phone" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Telefone"
+              type="tel"
+              name="telefone"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Email" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Email"
+              type="email"
+              name="email"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Endereço" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Endereço"
+              type="text"
+              name="endereco"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Raio de entrega" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Raio de entrega"
+              type="number"
+              name="raioEntrega"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Consumo mês" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Consumo mês"
+              type="number"
+              name="consumoMes"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Consumo fim de semana" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Consumo fim de semana"
+              type="number"
+              name="consumoFimDeSemana"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Valor médio" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Valor médio"
+              type="number"
+              name="valorMedio"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
 
-        <ThemeProvider theme={customTheme(outerTheme)}>
-          <TextField label="Canais de venda" />
-        </ThemeProvider>
+          <ThemeProvider theme={customTheme(outerTheme)}>
+            <TextField
+              label="Canais de venda"
+              type="text"
+              name="canaisVenda"
+              onChange={valorInput}
+            />
+          </ThemeProvider>
+          <br />
+          <FormRegisterButton type="submit">Enviar</FormRegisterButton>
+        </form>
       </Box>
-
-      <FormRegisterButton>Enviar</FormRegisterButton>
     </FormRegisterContainer>
   );
 }
